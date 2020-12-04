cb41ded028b0d92f7e3d8db1b1d7d5dfe70b419c
qatrainingjaguila <jaguila@academytrainee.com>
2020-12-04 23:05:20 +0000

    viewTickets cards

--- sfia3frontend/src/Components/Ticket.jsx
+++ sfia3frontend/src/Components/Ticket.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React from "react";
+import {Card, Button} from "react-bootstrap";
 
+const Ticket = (props) => {
+  const ticketinfo = props.info;
+  console.log(props.info);
 
-const Ticket = () => {
-return( 
-    <h1>A ticket</h1>
- );
-}
+  const date = new Date(Date.now())
+  let stringDate = date.toString();
+
+  return (
+    <>
+      <Card>
+        <Card.Header as="h5">{stringDate.slice(0,28)}</Card.Header>
+        <Card.Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>
+            {props.author}
+          </Card.Text>
+          <Button variant="primary">More Information</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
 export default Ticket;
--- sfia3frontend/src/Components/pages/viewTickets.jsx
+++ sfia3frontend/src/Components/pages/viewTickets.jsx
@@ -4,6 +4,7 @@ import { Col, Container } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 import SiteNavbar from '../SiteNavbar';
 import Ticket from '../Ticket';
+import Data from './ticket.json';
 
 const ViewTickets = () => {
 return(  
@@ -13,7 +14,16 @@ return(
     <Row>
         <Col className="shaded create" sm={4}>This is the side bar</Col>
         <Col className="shaded create"  sm={8}>
-            <Ticket/>
+            {Data.map((ticket)=>(
+            <Ticket 
+            key={ticket.id} 
+            title={ticket.title} 
+            author={ticket.author}
+            description={ticket.description}
+            timeCreated={ticket.timeCreated}
+            info={ticket}
+            />
+            ))}
         </Col>
     </Row>
 </Container>
