49759dbd39c402ea3f67dcb8bef822b6aa618fe7
Jon <jonathan.james.hunt@outlook.com>
2020-12-08 10:44:16 +0000

    added information overlay to description

--- sfia3frontend/src/Components/pages/createTicket.jsx
+++ sfia3frontend/src/Components/pages/createTicket.jsx
@@ -1,8 +1,22 @@
 import { useState } from 'react';
-import { Row, Col, Form, InputGroup, Button, Container } from 'react-bootstrap';
+import { Row, Col, Form, InputGroup, Button, Container, Tooltip, OverlayTrigger } from 'react-bootstrap';
 import SiteNavbar from '../SiteNavbar.jsx';
 import axios from 'axios';
 import { PATH } from '../../constants.json';
+
+const renderTooltip = (props) => (
+    <Tooltip id="description-tooltip" {...props}>
+        <p>Please provide a detailed description of your issue.</p>
+        <p>To ensure trainers can provide a quick and efficient response to your problem, please include the following:</p>
+
+        <p>1. When the issue occured</p>
+        <p>2. The OS(operating system) you are using.</p >
+        <p>3. When the issue first occured</p >
+        <p>4. The detail of what is happening.</p >
+        <p>5. What you have tried to fix it.</p >
+
+    </Tooltip>
+);
 
 const CreateTicket = () => {
     const [validated, setValidated] = useState(false);
@@ -42,113 +56,121 @@ const CreateTicket = () => {
 
     return (
         <>
-        <SiteNavbar />
-        <Container fluid>
-            <Row>
-                <Col className="shaded create" sm={4}>This is the side bar</Col>
-                <Col className="shaded create" sm={8}>
-                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                        <Form.Row>
-                            <Form.Group as={Col} md="4" controlId="validationCustom01">
-                                <Form.Label>First name</Form.Label>
-                                <Form.Control
-                                    required
-                                    name="firstName"
-                                    type="text"
-                                    placeholder="First name"
-                                    defaultValue="Mark"
-                                    value={firstName}
-                                    onInput={e => setfirstName(e.target.value)}
-                                />
-                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="4" controlId="validationCustom02">
-                                <Form.Label>Last name</Form.Label>
-                                <Form.Control
-                                    required
-                                    name="lastName"
-                                    type="text"
-                                    placeholder="Last name"
-                                    defaultValue="Otto"
-                                    value={lastName}
-                                    onInput={e => setlastName(e.target.value)}
-                                />
-                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="4" controlId="validationCustomUsername">
-                                <Form.Label>QA Email Address</Form.Label>
-                                <InputGroup>
-                                    <InputGroup.Prepend>
-                                        <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                                    </InputGroup.Prepend>
+            <SiteNavbar />
+            <Container fluid>
+                <Row>
+                    <Col className="shaded create" sm={4}>This is the side bar</Col>
+                    <Col className="shaded create" sm={8}>
+                        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                            <Form.Row>
+                                <Form.Group as={Col} md="4" controlId="validationCustom01">
+                                    <Form.Label>First name</Form.Label>
+                                    <Form.Control
+                                        required
+                                        name="firstName"
+                                        type="text"
+                                        placeholder="First name"
+                                        defaultValue="Mark"
+                                        value={firstName}
+                                        onInput={e => setfirstName(e.target.value)}
+                                    />
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+
+                                <Form.Group as={Col} md="4" controlId="validationCustom02">
+                                    <Form.Label>Last name</Form.Label>
+                                    <Form.Control
+                                        required
+                                        name="lastName"
+                                        type="text"
+                                        placeholder="Last name"
+                                        defaultValue="Otto"
+                                        value={lastName}
+                                        onInput={e => setlastName(e.target.value)}
+                                    />
+                                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="4" controlId="validationCustomUsername">
+                                    <Form.Label>QA Email Address</Form.Label>
+                                    <InputGroup>
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control
+                                            type="text"
+                                            name="email"
+                                            value={email}
+                                            onInput={e => setEmail(e.target.value)}
+                                            placeholder="...@academytrainee.com"
+                                            aria-describedby="inputGroupPrepend"
+                                        // required
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Please choose a username.
+            </Form.Control.Feedback>
+                                    </InputGroup>
+                                </Form.Group>
+                            </Form.Row>
+
+                            <Form.Row>
+                                <Form.Group as={Col} md="6" controlId="validationCustom04">
+                                    <Form.Label>Title</Form.Label>
                                     <Form.Control
                                         type="text"
-                                        name="email"
-                                        value={email}
-                                        onInput={e => setEmail(e.target.value)}
-                                        placeholder="...@academytrainee.com"
-                                        aria-describedby="inputGroupPrepend"
-                                    // required
-                                    />
+                                        placeholder="Title"
+                                        required
+                                        name="title"
+                                        value={title}
+                                        onInput={e => setTitle(e.target.value)} />
+                                    <Form.Control.Feedback type="invalid" >
+                                        Please provide a valid title.
+          </Form.Control.Feedback>
+                                </Form.Group>
+                                <Form.Group as={Col} md="6" controlId="validationCustom05">
+                                    <Form.Label>Urgency</Form.Label>
+                                    <Form.Control
+                                        as="select"
+                                        required
+                                        name="urgency"
+                                        value={urgency}
+                                        onInput={e => setUrgency(e.target.value)}>
+                                        <option value="low">Low (Not affecting your ability to work)</option>
+                                        <option value="medium">Medium (Somewhat affecting your ability to work)</option>
+                                        <option value="high">High (Critically affecting your ability to work)</option>
+                                    </Form.Control>
                                     <Form.Control.Feedback type="invalid">
-                                        Please choose a username.
-            </Form.Control.Feedback>
-                                </InputGroup>
-                            </Form.Group>
-                        </Form.Row>
+                                        Please provide a valid urgency level.
+          </Form.Control.Feedback>
+                                </Form.Group>
+                            </Form.Row>
+                            <Form.Row>
+                                <Form.Group as={Col} md="12" controlId="validationCustom03">
+                                    <Form.Label>Description</Form.Label>
+                                    <OverlayTrigger
+                                        placement="left"
+                                        delay={{ show: 250, hide: 400 }}
+                                        overlay={renderTooltip}
+                                    >
+                                        <Form.Control
+                                            as="textarea"
+                                            placeholder="Please provide a description of your issue...."
+                                            required
+                                            name="description"
+                                            value={description}
+                                            onInput={e => setDescription(e.target.value)}></Form.Control>
+                                    </OverlayTrigger>
+                                    <Form.Control.Feedback type="invalid">
+                                        Please provide a valid Description.
+          </Form.Control.Feedback>
 
-                        <Form.Row>
-                            <Form.Group as={Col} md="6" controlId="validationCustom04">
-                                <Form.Label>Title</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Title"
-                                    required
-                                    name="title"
-                                    value={title}
-                                    onInput={e => setTitle(e.target.value)} />
-                                <Form.Control.Feedback type="invalid" >
-                                    Please provide a valid title.
-          </Form.Control.Feedback>
-                            </Form.Group>
-                            <Form.Group as={Col} md="6" controlId="validationCustom05">
-                                <Form.Label>Urgency</Form.Label>
-                                <Form.Control
-                                    as="select"
-                                    required
-                                    name="urgency"
-                                    value={urgency}
-                                    onInput={e => setUrgency(e.target.value)}>
-                                    <option value="low">Low (Not affecting your ability to work)</option>
-                                    <option value="medium">Medium (Somewhat affecting your ability to work)</option>
-                                    <option value="high">High (Critically affecting your ability to work)</option>
-                                </Form.Control>
-                                <Form.Control.Feedback type="invalid">
-                                    Please provide a valid urgency level.
-          </Form.Control.Feedback>
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row>
-                            <Form.Group as={Col} md="12" controlId="validationCustom03">
-                                <Form.Label>Description</Form.Label>
-                                <Form.Control
-                                    as="textarea"
-                                    placeholder="Please provide a description of your issue...."
-                                    required
-                                    name="description"
-                                    value={description}
-                                    onInput={e => setDescription(e.target.value)} />
-                                <Form.Control.Feedback type="invalid">
-                                    Please provide a valid Description.
-          </Form.Control.Feedback>
-                            </Form.Group>
-                        </Form.Row>
-                        <Button type="submit">Submit Issue</Button>
-                    </Form>
+                                </Form.Group>
+                            </Form.Row>
+                            <Button type="submit">Submit Issue</Button>
+                        </Form>
 
-                </Col>
-            </Row>
-        </Container>
+                    </Col>
+                </Row>
+            </Container>
         </>
     );
 
