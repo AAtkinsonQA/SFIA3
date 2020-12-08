cc38a92ff49ecad4099b954834fd8f6406d4d91d
Jon <jonathan.james.hunt@outlook.com>
2020-12-08 15:19:52 +0000

    added delete button

--- sfia3frontend/src/Components/Ticket.jsx
+++ sfia3frontend/src/Components/Ticket.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import {Card, Button} from "react-bootstrap";
+import React, { useImperativeHandle } from "react";
+import { Card, Button } from "react-bootstrap";
+import axios from 'axios';
+import { PATH } from '../constants.json';
 
 const Ticket = (props) => {
   const ticketinfo = props.info;
@@ -7,26 +9,31 @@ const Ticket = (props) => {
   const date = new Date(Date.now()) //temporary date
   let stringDate = date.toString();
 
-  const updateState=props.updateState;
+  const updateState = props.updateState;
   console.log(updateState);
 
   let id = props.id;
   console.log(props.id);
   console.log(id);
 
-
+  const handleSubmit = (event) => {
+    axios.delete(PATH + '/deleteTicket/' + props.id).then(response => {
+      console.log(response);
+      window.location.reload(false);
+    });
+  }
   return (
     <>
       <Card>
-        <Card.Header as="h5">{stringDate.slice(0,28)}</Card.Header>
+        <Card.Header as="h5">{stringDate.slice(0, 28)}</Card.Header>
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>
             {props.author}
           </Card.Text>
           <div>
-          <Button style = {{marginRight:"2em"}} value={id} variant="primary" onClick={updateState}>More Information</Button>
-          <Button variant="danger">Delete</Button>
+            <Button style={{ marginRight: "2em" }} value={id} variant="primary" onClick={updateState}>More Information</Button>
+            <Button onClick={handleSubmit} variant="danger">Delete</Button>
           </div>
         </Card.Body>
       </Card>
