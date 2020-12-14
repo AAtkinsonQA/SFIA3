9141db3071426109cce06bcf01c97dcde12ce339
qatrainingjaguila <jaguila@academytrainee.com>
2020-12-14 18:08:06 +0000

    kube

--- scripts/deploykube.sh
+++ scripts/deploykube.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 cd kube
-# kubectl create ns pipeline
-# kubectl apply -f nginx-config.yaml -n pipeline
-# kubectl apply -f secret.yaml -n pipeline
-# kubectl apply -f backend.yaml -f frontend.yaml -f nginx.yaml
-# kubectl describe service/nginx-lb -n pipeline
 
 #kubectl rollout restart deployment/frontend
 #kubectl rollout restart deployment/microservices
