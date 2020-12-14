f641d4275b36dd7157f6cf0e03956258bb000c40
qatrainingjaguila <jaguila@academytrainee.com>
2020-12-14 20:27:56 +0000

    test

--- scripts/deploykube.sh
+++ scripts/deploykube.sh
@@ -5,5 +5,6 @@ cd kube
 #kubectl rollout restart deployment/frontend
 #kubectl rollout restart deployment/microservices
 sudo kubectl apply -f frontend.yaml -f create.yaml -f delete.yaml -f get.yaml -f getsingle.yaml -f update.yaml
-sudo kubectl apply -f nginx.yaml -f nginx-conf.yaml
+sudo kubectl apply -f nginx-conf.yaml 
+sudo kubectl apply -f nginx.yaml
 kubectl describe service/nginx-lb
