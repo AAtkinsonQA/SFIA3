0389b3b2f390cb3435361f7dc352da871464d81b
qatrainingjaguila <jaguila@academytrainee.com>
2020-12-14 19:11:22 +0000

    frontend

--- scripts/buildfrontend.sh
+++ scripts/buildfrontend.sh
@@ -5,7 +5,7 @@ git clone --branch JenkinsTest https://github.com/qatrainingjaguila/SFIA3.git
 cd SFIA3/sfia3frontend
 
 
-docker build -t qatrainingjaguila/sfia3-frontend:latest
-docker push qatrainingjaguila/sfia3-frontend:latest
+sudo docker build -t qatrainingjaguila/sfia3-frontend:latest .
+sudo docker push qatrainingjaguila/sfia3-frontend:latest
 #docker build -t nexus-IP:repo-port/sfia3-frontend:latest
 #docker push nexus-IP:repo-port/sfia3-frontend:latest
